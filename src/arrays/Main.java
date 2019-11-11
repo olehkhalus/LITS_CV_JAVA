fd261718981dc700ba0fc2680e3c191d6dde8712
Oleh Khalus <oleh0704@gmail.com>
2019-11-11 17:15:31 +0200

    Array tasks

---
+++ src/arrays/Main.java
@@ -0,0 +1,10 @@
+package arrays;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        ArraysTasks arraysTasks = new ArraysTasks();
+        arraysTasks.numberOfTask();
+    }
+}
